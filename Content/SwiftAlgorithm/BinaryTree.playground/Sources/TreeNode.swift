bb5594726146dcdb71748dc4f8cf939bfe973846
Owenli <jesus_lqq@163.com>
2018-04-25 13:49:40 +0800

    add Binary Tree

---
+++ Content/SwiftAlgorithm/BinaryTree.playground/Sources/TreeNode.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+    }
+}
