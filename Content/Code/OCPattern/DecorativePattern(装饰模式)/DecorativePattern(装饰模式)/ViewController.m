a1bb1d679012a759f01b7641b68377ff25b74ac0
Owenli <jesus_lqq@163.com>
2018-05-14 17:05:40 +0800

    add Code

--- Content/Code/OCPattern/DecorativePattern(装饰模式)/DecorativePattern(装饰模式)/ViewController.m
+++ Content/Code/OCPattern/DecorativePattern(装饰模式)/DecorativePattern(装饰模式)/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "ImageTransformFilter.h"
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -17,5 +20,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.image  = [UIImage imageNamed:@"p.jpg"];
+    
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 199, 100, 80)];
+    [self.view addSubview:self.imageView];
+    [self transformImage];
 }
+
+- (void)transformImage {
+    CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI_4);
+    
+    id <ImageComponent> finalImage = [[ImageTransformFilter alloc] initWithImageComponent:self.image transform:transform];
+    self.imageView.image = finalImage;
+}
+
 @end
